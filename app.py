9beb83221a470f85f1245a4e61b4f7d556234fd1
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-03-12 13:34:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ st.title ("LOC")
 
 st.header ("En este espacio podrás obtener información de La Otra Ciudad")
 
-image=image.open("LOC_Logo_04.jpg"
+image=image.open("LOC_Logo_04.jpg")
+st.image(image, caption="La Otra Ciudad, Logo")
+         st.write("Colectivo de jóvenes, very well")

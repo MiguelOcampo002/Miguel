2e686776b62f0a98ba86774230cc644ff7be51d2
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-03-12 13:33:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-st.title ("Mi primer proyecto") 
+st.title ("LOC") 
 
+st.header ("En este espacio podrás obtener información de La Otra Ciudad")
+
+image=image.open("LOC_Logo_04.jpg"
